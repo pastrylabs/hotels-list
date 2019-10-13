29720903d353b01d661e0352cb5155b9a10b9fd7
praveensastry <sastry.praveen@gmail.com>
2019-10-13 15:46:57 +1100

    Add storybook, tests and lintitng

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import App from "../App";
+import React from "react";
+
+it("renders welcome message", () => {
+  const { getByText } = render(<App />);
+  expect(getByText("Learn React")).toBeInTheDocument();
+});
